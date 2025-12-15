4d2e84532a684c90d8a629b4ce095aa934310e80
Tcrisw094 <TCRISW9754@my.sullivan.edu>
2025-12-15 10:24:47 -0600

    Tic-Tac-Toe

--- WindowsFormsApp2/CSC240-08-02-ViewInvoices.cs
+++ WindowsFormsApp2/CSC240-08-02-ViewInvoices.cs
@@ -5,12 +5,14 @@ namespace WindowsFormsApp2
 {
     public partial class CSC240_08_02_ViewInvoices : Form
     {
+        private object dgvInvoices;
+
         public CSC240_08_02_ViewInvoices()
         {
             InitializeComponent();
             // demo data
             dgvInvoices.Rows.Add("1001", "2025-12-01", "150.00");
-            dgvInvoices.Rows.Add("1002", "2025-12-05", "200.50");
+            object addedInvoiceRowIndex = dgvInvoices.Rows.Add("1002", "2025-12-05", "200.50");
         }
     }
 }
